a56969bac94def268257f9b5a9fe5780da6e0aa0
Thiago de Oliveira Dantas <t.dod@live.com>
2021-07-15 16:41:06 -0300

    MANIPULAÇÃO DE IMAGEM

--- MANIPULACAOIMG/index.php
+++ MANIPULACAOIMG/index.php
@@ -1,10 +1,31 @@
 <?php
+//PRECISA ATIVAR A BIBLIOTECA GD NO PHP.INI
 
-$imagem = imagecreatetruecolor(300, 300);
-$cor = imagecolordeallocate($imagem, 255, 0, 0);
+$arquivo = 'paisagem.jpeg';
+$maxWidth = 1980;
+$maxHeight = 1080;
+
+list($originalWidth, $originalHeight) = getimagesize($arquivo);
+
+$ratio = $originalWidth / $originalHeight;
+$ratioDest = $maxWidth / $maxHeight;
+
+$finalWidth = 0;
+$finalHeight = 0;
+
+if($ratioDest > $ratio){
+    $finalWidth = $maxHeight * $ratio;
+    $finalHeight = $maxHeight;
+}else{
+    $finalHeight = $maxWidth / $ratio;
+    $finalWidth = $maxWidth;
+}
 
 
-imagefill($imagem, 0, 0, $cor);
+$imagem = imagecreatetruecolor($finalWidth, $finalHeight);
+$originalImg = imagecreatefromjpeg($arquivo);
+
+imagecopyresampled($imagem, $originalImg, 0, 0, 0, 0, $finalWidth, $finalHeight, $originalWidth, $originalHeight);
 
 header("Content-Type: image/jpeg");
 
