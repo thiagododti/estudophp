7d6296054afb9cd10a8dd8a3940328373585e66e
Thiago de Oliveira Dantas <t.dod@live.com>
2021-07-16 16:46:53 -0300

    MODELO_MVC_ESTUDO

---
+++ mvc/src/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use core\Router;
+
+$router = new Router();
+
+$router->get('/', 'HomeController@index');
+$router->get('/sobre/{nome}', 'HomeController@sobreP');
+$router->get('/sobre', 'HomeController@sobre');
---
+++ mvc/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd8c59cf1663a3f7b09a3dd08b6caa0895b87bd31',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd8c59cf1663a3f7b09a3dd08b6caa0895b87bd31',
+            'dev_requirement' => false,
+        ),
+        'clancats/hydrahon' => array(
+            'pretty_version' => 'v1.1.12',
+            'version' => '1.1.12.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../clancats/hydrahon',
+            'aliases' => array(),
+            'reference' => 'cc2fc3fd2ea082a2d531a751da49cc6a413270cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
