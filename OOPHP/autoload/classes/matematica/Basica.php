7f6f26fcfc7803483e847c9a6c66de5860bd0166
Thiago de Oliveira Dantas <t.dod@live.com>
2021-07-30 16:16:16 -0300

    autoload

---
+++ OOPHP/autoload/classes/matematica/Basica.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace \matematica;
+
+class Basica
+{
+
+    public function somar($x, $y)
+    {
+        return $x + $y;
+    }
+}
---
+++ OOPHP/autoload/index-autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'autoloader.php';
+
+
+$m = new Matematica();
+echo $m->somar(10, 20);
